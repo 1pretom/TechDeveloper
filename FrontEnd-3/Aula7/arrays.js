06cc3d99800901232c7b6a9825f3c473ede91a7f
Washington <washingtonldamacenac@gmail.com>
2023-03-06 22:14:59 -0300

    aula e mesa 7

---
+++ FrontEnd-3/Aula7/arrays.js
@@ -0,0 +1,8 @@
+//ponteiro para imutabilidade
+
+const numeros = [10,20,30,40,50]
+const copiaNumeros = [...numeros,60,70];
+
+/* copiaNumeros.push(60) */
+console.log(numeros)
+console.log(copiaNumeros)
--- FrontEnd-3/Aula7/aula7/src/App.jsx
+++ FrontEnd-3/Aula7/aula7/src/App.jsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
 
 function App() {
+    const [number, setNumber] = useState([])
+    
+    
+
+    function addNumber() {
+        setNumber([...number, number.length])
+        /* setNumber([...number,60,70]) */
+        
+        /* number++; */
+      /*   const copyNumbers = [...number]
+        copyNumbers.push(60)
+        setNumber(copyNumbers) */
+    }
+
+    return <div>
+        <h1>numbers</h1>
+        <ul>
+            {number.map((number,index)=>(
+                <li key = {index}>{number}</li>
+            ))}
+        </ul>
+        
+        <button onClick={addNumber}>click here</button>
+    </div>
 }
 
 
-export default App
+export default App;
